d50d3f728395a32822ed91e6fd983d20399fb53f
Boris Krasko <boris.krasko@icloud.com>
2020-10-10 00:45:31 +0200

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		let brackets = bracketsConfig[i].join('');
+		while (str.includes(brackets)) {
+			str = str.replace(brackets, '');
+			i = -1;
+		}
+	}
+	if (str) {
+		return 0;
+	} else {
+		return 1;
+	}
 }
